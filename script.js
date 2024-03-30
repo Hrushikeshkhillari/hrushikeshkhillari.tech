8037728294b97f0a074ea3555eac368d737781b7
HrushiKhillari <hrushikhillari@gmail.com>
2024-03-30 14:45:27 +0530

    portfoliio
    code

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simulated logs
+var logs = "Error: Uncaught TypeError: Cannot read property 'submit' of null\n    at submitForm (script.js:10)\n    at HTMLButtonElement.onclick (index.html:9)";
+
+// Display logs in pre element
+document.getElementById("logs").textContent = logs;
+
