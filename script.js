78bf6a3aa7942da42e0aae2405d74d20c2d3d249
Hrushikesh Khillari <128903902+Hrushikeshkhillari@users.noreply.github.com>
2024-09-05 22:01:07 +0530

    update file

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simulated logs
-var logs = "Error: Uncaught TypeError: Cannot read property 'submit' of null\n    at submitForm (script.js:10)\n    at HTMLButtonElement.onclick (index.html:9)";
-
-// Display logs in pre element
-document.getElementById("logs").textContent = logs;
-
-// for the dark mode 
